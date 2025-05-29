767a7316fb56a49a273ef3e6b2a2f345010eeece
FrancescoCellitti <francescocellitti@virgilio.it>
2025-05-29 16:36:09 +0200

    Refactor teacher removal and presence check logic in script.js

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -26,14 +26,26 @@ console.log(longNames);
 
 
 // 3. Rimuovi 'Ed' dall'array teachers
-delete teachers[1];
-console.log(teachers);
+teachers.splice(5,1);
+console.log(teachers)
+
 
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = teachers.includes('Fabio');
-console.log(isFabioPresent);
+let isFabioPresent = null;
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === 'Fabio') {
+    isFabioPresent = true;
+    break;
+
+  } else {
+    isFabioPresent = false;
+
+  }
+}
+console.log(isFabioPresent)
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.toString();
